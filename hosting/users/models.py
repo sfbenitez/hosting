a1d4aeab032b5d9ee3febc4b08d9e7715d23753e
Ferrete <sergioferretebenitez@gmail.com>
2017-12-21 14:51:12 +0100

    fixed foreign key from roles to users

--- hosting/users/models.py
+++ hosting/users/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.utils import timezone
 
+class roles(models.Model):
+    rol_id = models.IntegerField()
+    description = models.CharField(max_length=200)
+
 class Users(models.Model):
     user = models.CharField(max_length=20)
     pg_password = models.CharField(max_length=50)
     ftp_password = models.CharField(max_length=50)
-    rol_id = models.IntegerField(max_length=2, default=1)
+    rol_id = models.ForeignKey(roles, on_delete=models.CASCADE)
     fecha_alta = models.DateTimeField(default=timezone.now)
-
-class roles(models.Model):
-    rol_id = models.IntegerField(max_length=2)
-    description = models.CharField(max_length=200)
